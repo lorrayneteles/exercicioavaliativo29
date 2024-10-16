5725005055986c66059a3d17a6ea6bd662ea2fe4
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 19:39:29 -0300

    Add files via upload

---
+++ exercicioavaliativo29.py
@@ -0,0 +1,6 @@
+#peça dois numros e mostre o resultado da divisao com apenas duas casas decimais
+
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite outro numero:'))
+divisao = num1 / num2
+print(f'A divisao de {num1} por {num2} é {divisao:.2f}')
